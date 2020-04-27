2226acf0b7b835002767176dd43f77dacee64ea7
Ahmad Ali <aallii300300@gmail.com>
2020-04-27 21:27:39 +0100

    redigsn functons

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 window.i = 1;
 let stop = false;
 var carTime;
-var firstSlide=1;
+var firstSlide = 1;
 var lastSlide = 4;
 var slidertime = 5000;
 
 function carousel() {
+    //change pause/start button
     const sbtn = document.getElementById('sbtn');
-    !stop ? sbtn.innerText ="||" : sbtn.innerText="|>";
-
+    !stop ? sbtn.innerText = "||" : sbtn.innerText = "|>";
+    
     const carImg = document.getElementById('carImg');
     if (window.i <= 0) { window.i = lastSlide; }
     if (window.i > lastSlide) { window.i = 1; }
 
-    if ( !stop &&  window.i < lastSlide +1) {
+    if (!stop && window.i < lastSlide + 1) {
+        /**
+         * adapt slider if the image is jpg, jpeg or png
+         */
         // let i = window.i;
         let file1 = doesFileExist(`/imgs/${window.i}.png`);
 
-
         // console.log("file 1", file1)
         // console.log("file 2", file2)
-
 
         if (file1) { carImg.setAttribute('src', '/imgs/' + window.i + '.png'); }
         else {
@@ -32,16 +34,12 @@ function carousel() {
 
         }
 
-
-
-       
-        
         window.i = window.i + 1;
         console.log('i++', i);
-        carTime =  setTimeout(carousel, slidertime);
+        carTime = setTimeout(carousel, slidertime);
     }
 
-   
+
 
 }
 
@@ -67,94 +65,72 @@ const nbtn = document.getElementById('nbtn');
 const sbtn = document.getElementById('sbtn');
 
 
-pbtn.addEventListener('click', ()=>{
-    if(stop == true){ stop = false; }
-    // console.log('stio changed to   ', stop);
-    window.i= window.i - 2;
-    // console.log('before if' , window.i )
-    if(window.i < 1) {  
-        window.i = lastSlide; 
-        // console.log('inside if' , window.i )
-        clearTimeout(carTime);
-        return carousel();
-     }
-    //window.i= window.i - 2;
-    // console.log('out if' , window.i )
-    clearTimeout(carTime);
-     carousel();
+pbtn.addEventListener('click', () => {
+    nextSlide();
 });
 
-nbtn.addEventListener('click', ()=>{
-    if(stop == true){ stop = false; }
-    // window.i= window.i + 1;
-    if(window.i == lastSlide+1) {  
-        window.i = 1; 
-        clearTimeout(carTime);
-        return carousel(); 
+nbtn.addEventListener('click', () => {
+   perviousSlide();
+});
+
+sbtn.addEventListener('click', () => {
+    stopSlider();
+});
+
+
+window.addEventListener('keydown', (e) => {
+    let key = e.key;
+    if (key == 'ArrowRight') {
+        nextSlide();
     }
-   
-    clearTimeout(carTime);
-     carousel();
-});
 
-sbtn.addEventListener('click', ()=>{
-   stop = !stop;
-   clearTimeout(carTime);
-   carousel();
-});
-
-
-window.addEventListener('keydown', (e)=>{
-
-let key = e.key;
-console.log(key);
-
-if(key == 'ArrowRight'){
-
-    if(stop == true){ stop = false; }
-    // window.i= window.i + 1;
-    if(window.i == lastSlide+1) {  
-        window.i = 1; 
-        clearTimeout(carTime);
-        return carousel(); 
+    if (key == 'ArrowLeft') {
+        perviousSlide();
     }
-   
-    clearTimeout(carTime);
-     carousel();
-
-}
-
-if(key == 'ArrowLeft'){
-
-    if(stop == true){ stop = false; }
-    // console.log('stio changed to   ', stop);
-    window.i= window.i - 2;
-    // console.log('before if' , window.i )
-    if(window.i < 1) {  
-        window.i = lastSlide; 
-        // console.log('inside if' , window.i )
-        clearTimeout(carTime);
-        return carousel();
-     }
-    //window.i= window.i - 2;
-    // console.log('out if' , window.i )
-    clearTimeout(carTime);
-     carousel();
-
-}
 
 
-if(key == " "){
-
-    stop = !stop;
-   clearTimeout(carTime);
-   carousel();
-
-}
-
+    if (key == " ") {
+        stopSlider();
+    }
 })
 
 
+function stopSlider() {
+    stop = !stop;
+    clearTimeout(carTime);
+    carousel();
+
+}
+
+function perviousSlide() {
+    if (stop == true) { stop = false; }
+    // console.log('stio changed to   ', stop);
+    window.i = window.i - 2;
+    // console.log('before if' , window.i )
+    if (window.i < 1) {
+        window.i = lastSlide;
+        // console.log('inside if' , window.i )
+        clearTimeout(carTime);
+        return carousel();
+    }
+    //window.i= window.i - 2;
+    // console.log('out if' , window.i )
+    clearTimeout(carTime);
+    carousel();
+}
+
+function nextSlide() {
+    if (stop == true) { stop = false; }
+    // window.i= window.i + 1;
+    if (window.i == lastSlide + 1) {
+        window.i = 1;
+        clearTimeout(carTime);
+        return carousel();
+    }
+
+    clearTimeout(carTime);
+    carousel();
+}
 
 
 window.onload = carousel;
